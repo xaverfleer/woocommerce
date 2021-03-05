a340ff65c200f175f9d8ff0eef5510803c2e328a
Jonathan Sadowski <sadowski@automattic.com>
2021-03-05 11:25:13 -0600

    Update select2.js to use String.prototype.trim instead of jQuery.trim

--- assets/js/select2/select2.js
+++ assets/js/select2/select2.js
@@ -3588,7 +3588,7 @@ S2.define('select2/data/tags',[
   };
 
   Tags.prototype.createTag = function (decorated, params) {
-    var term = $.trim(params.term);
+    var term = params.term.trim();
 
     if (term === '') {
       return null;
@@ -4816,7 +4816,7 @@ S2.define('select2/defaults',[
 
     function matcher (params, data) {
       // Always return the object if there is nothing to compare
-      if ($.trim(params.term) === '') {
+      if ( params.term.trim() === '' ) {
         return data;
       }
 
