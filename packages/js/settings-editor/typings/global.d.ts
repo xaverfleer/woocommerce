1675618a26dc8d2d1026e14d252b9bef80df30c0
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-11-21 07:08:36 +0800

    [Settings] Add transformer for grouping settings elements into sections and checkbox groups (#52905)
    * Transform SettingsSection data for layout
    
    * Add changelog
    
    * Fix doc
    
    * Improve code and tests
    
    * Update global.d.ts
    
    * Add changelog
    
    * Optimize array_merge
    
    * Refactor from static to instance-based class
    
    * Remove duplciate type
    
    * Add test cases for handling null inputs in Transformer

--- packages/js/settings-editor/typings/global.d.ts
+++ packages/js/settings-editor/typings/global.d.ts
@@ -1,27 +1,42 @@
 declare global {
-	interface SettingsField {
-		title?: string;
-		type: string;
-		id?: string;
-		desc?: string;
-		desc_tip?: boolean | string;
-		default?: string | number | boolean | object;
-		value: string | number | boolean | object;
-		placeholder?: string;
-		custom_attributes?: {
-			[ key: string ]: string | number;
-		};
-		options?: {
-			[ key: string ]: string;
-		};
-		css?: string;
-		class?: string;
-		checkboxgroup?: 'start' | 'end' | '';
-		autoload?: boolean;
-		show_if_checked?: string;
-		content?: string;
-		[ key: string ]: any;
-	}
+    interface BaseSettingsField {
+        title?: string;
+        type: string;
+        id?: string;
+        desc?: string;
+        desc_tip?: boolean | string;
+        default?: string | number | boolean | object;
+        value: string | number | boolean | object;
+        placeholder?: string;
+        custom_attributes?: {
+            [key: string]: string | number;
+        };
+        options?: {
+            [key: string]: string;
+        };
+        css?: string;
+        class?: string;
+        autoload?: boolean;
+        show_if_checked?: string;
+        content?: string;
+        [key: string]: any;
+    }
+
+    interface GroupSettingsField extends BaseSettingsField {
+        type: 'group';
+        settings: SettingsField[];
+    }
+    interface CheckboxSettingsField extends BaseSettingsField {
+        type: 'checkbox';
+        checkboxgroup?: 'start' | 'end' | '';
+    }
+
+    interface CheckboxGroupSettingsField extends BaseSettingsField {
+        type: 'checkboxgroup';
+        settings: CheckboxSettingsField[];
+    }
+
+    type SettingsField = BaseSettingsField | GroupSettingsField | CheckboxGroupSettingsField | CheckboxSettingsField;
 
 	interface SettingsSection {
 		label: string;
--- plugins/woocommerce/src/Admin/Features/Settings/Init.php
+++ plugins/woocommerce/src/Admin/Features/Settings/Init.php
@@ -1,20 +1,22 @@
-<?php //phpcs:ignore Generic.PHP.RequireStrictTypes.MissingDeclaration
+<?php
 /**
  * WooCommerce Settings.
  */
 
-namespace Automattic\WooCommerce\Admin\Features;
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\Admin\Features\Settings;
 
 use Automattic\WooCommerce\Internal\Admin\WCAdminAssets;
 
 /**
  * Contains backend logic for the Settings feature.
  */
-class Settings {
+class Init {
 	/**
 	 * Class instance.
 	 *
-	 * @var Settings instance
+	 * @var Init instance
 	 */
 	protected static $instance = null;
 
@@ -145,8 +147,8 @@ class Settings {
 		foreach ( $setting_pages as $setting_page ) {
 			$pages = $setting_page->add_settings_page_data( $pages );
 		}
-
-		$settings['settingsData'] = $pages;
+		$transformer              = new Transformer();
+		$settings['settingsData'] = $transformer->transform( $pages );
 
 		return $settings;
 	}
