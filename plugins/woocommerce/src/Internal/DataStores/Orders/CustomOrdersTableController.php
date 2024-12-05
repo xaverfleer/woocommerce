a4a055de3baff26941dfbb475b6aadfcbedc582e
Michael Pretty <prettyboymp@users.noreply.github.com>
2024-12-05 13:54:18 -0500

    Adding OrderTableDatastore and Meta caching integration (#46023)
    Adding a feature for caching of raw order data at the OrderDataStore level.
    
    Closes #45550
    
    With this feature enabled:
    * The cache group used by OrderCache is moved to `order_objects` and made non-persistent
    * The OrderTableDataStore and OrderTableDataStoreMeta now cache retrieved data.
    * Cache for individual order data is purged automatically after a write operation to the order.
    
    This is all feature gated behind the `woocommerce_hpos_datastore_caching_enabled` option and is currently under alpha testing - meaning that the option to enable this feature is only available if `WOOCOMMERCE_ENABLE_ALPHA_FEATURE_TESTING` is defined as true.

--- plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
@@ -53,6 +53,8 @@ class CustomOrdersTableController {
 
 	public const HPOS_FTS_ORDER_ITEM_INDEX_CREATED_OPTION = 'woocommerce_hpos_order_item_fts_index_created';
 
+	public const HPOS_DATASTORE_CACHING_ENABLED_OPTION = 'woocommerce_hpos_datastore_caching_enabled';
+
 	/**
 	 * The data store object to use.
 	 *
@@ -145,6 +147,7 @@ class CustomOrdersTableController {
 		add_filter( 'removable_query_args', array( $this, 'register_removable_query_arg' ) );
 		add_action( 'woocommerce_register_feature_definitions', array( $this, 'add_feature_definition' ) );
 		add_filter( 'get_edit_post_link', array( $this, 'maybe_rewrite_order_edit_link' ), 10, 2 );
+		add_action( 'before_woocommerce_init', array( $this, 'maybe_set_order_cache_group_as_non_persistent' ) );
 	}
 
 	/**
@@ -194,6 +197,16 @@ class CustomOrdersTableController {
 	 */
 	public function custom_orders_table_usage_is_enabled(): bool {
 		return get_option( self::CUSTOM_ORDERS_TABLE_USAGE_ENABLED_OPTION ) === 'yes';
+	}
+
+	/**
+	 * Is caching of data within the CustomerOrdersTable datastores enabled?
+	 *
+	 * @return bool True if the caching is enabled within the CustomeOrderTable Datastores.
+	 */
+	public function hpos_data_caching_is_enabled(): bool {
+		return get_option( self::HPOS_DATASTORE_CACHING_ENABLED_OPTION ) === 'yes' &&
+			$this->custom_orders_table_usage_is_enabled();
 	}
 
 	/**
@@ -315,6 +328,9 @@ class CustomOrdersTableController {
 	public function process_updated_option( $option, $old_value, $value ) {
 		if ( DataSynchronizer::ORDERS_DATA_SYNC_ENABLED_OPTION === $option && 'no' === $value ) {
 			$this->data_synchronizer->cleanup_synchronization_state();
+		}
+		if ( self::HPOS_DATASTORE_CACHING_ENABLED_OPTION === $option && $old_value !== $value && 'yes' === $value ) {
+			$this->data_store->clear_all_cached_data();
 		}
 	}
 
@@ -784,5 +800,20 @@ class CustomOrdersTableController {
 		}
 
 		return $link;
+	}
+
+	/**
+	 * Set the `order_objects` cache group as non-persistent if Custom Order data caching is enabled.
+	 *
+	 * With order datastore cache enabled, caching of raw data is now handled by the datastore, rather than full object
+	 * being stored in persistent cache.
+	 *
+	 * @return void
+	 */
+	public function maybe_set_order_cache_group_as_non_persistent() {
+		if ( OrderUtil::custom_orders_table_datastore_cache_enabled() ) {
+			// If we're using datastore cache, we don't want to persist the order objects in cache. It should be in-memory only.
+			wp_cache_add_non_persistent_groups( array( $this->order_cache->get_object_type() ) );
+		}
 	}
 }
