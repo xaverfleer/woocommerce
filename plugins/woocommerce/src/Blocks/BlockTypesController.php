077b0eb4086d1a18dcf4ec4484b037f18e1c258d
Luigi Teschio <gigitux@gmail.com>
2024-12-12 11:41:02 +0100

    Avoid crash widget area when Product Collection is rendered (#53656)
    * Avoid crash widget area when Product Collection is rendered
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/src/Blocks/BlockTypesController.php
+++ plugins/woocommerce/src/Blocks/BlockTypesController.php
@@ -481,8 +481,8 @@ final class BlockTypesController {
 					'Cart',
 					'Checkout',
 					'ProductGallery',
-					'ProductCollection',
-					'ProductCollectionNoResults',
+					'ProductCollection\Controller',
+					'ProductCollection\NoResults',
 				)
 			);
 		}
