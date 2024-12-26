4adb9bebe7725ec7e24c53c3a073fb8fb016873d
Veljko V. <veljano@yahoo.com>
2024-12-26 10:47:24 +0100

    Update e2e test util for filling page title to close expanded block inserter if present (#54031)
    * Update e2e editor util and fill page title inside
    
    * Add changelog

--- plugins/woocommerce/tests/e2e-pw/utils/editor.js
+++ plugins/woocommerce/tests/e2e-pw/utils/editor.js
@@ -53,6 +53,19 @@ const goToPostEditor = async ( { page } ) => {
 };
 
 const fillPageTitle = async ( page, title ) => {
+	// Gutenberg 19.9-nightly: Block Inserter expanded by default and showing patterns tab.
+	// TODO (Gutenberg 19.9.1+): Remove conditional and make this explicit.
+	if (
+		await page
+			.getByRole( 'button', {
+				name: /Toggle block inserter|Block Inserter/,
+				expanded: true,
+			} )
+			.isVisible()
+	) {
+		await page.getByLabel( 'Close Block Inserter' ).click();
+	}
+
 	// TODO (Gutenberg 19.9): Keep only the "Block: Title" label locator.
 	// Current stable version of Gutenberg (19.7) uses the "Add title" label locator.
 	// Upcoming Gutenberg 19.9 uses the "Block: Title" one. We should use it instead when GB 19.9 comes out.
