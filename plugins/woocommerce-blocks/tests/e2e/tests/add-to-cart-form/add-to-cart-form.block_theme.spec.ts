e738cb2cae056d00ffbfec9ad4e79cd596660a8f
Luigi Teschio <gigitux@gmail.com>
2025-01-02 17:37:49 +0100

    Add To Cart With Options: enable stepper layout (#54116)
    * Add To Cart With Options: enable stepper layout
    
    * Add changefile(s) from automation for the following project(s): woocommerce-blocks, woocommerce, woocommerce/client/admin
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-blocks/tests/e2e/tests/add-to-cart-form/add-to-cart-form.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/add-to-cart-form/add-to-cart-form.block_theme.spec.ts
@@ -213,12 +213,6 @@ test.describe( `${ blockData.name } Block`, () => {
 	} );
 
 	test.describe( 'Stepper Layout', () => {
-		test.beforeEach( async ( { requestUtils } ) => {
-			await requestUtils.setFeatureFlag(
-				'add-to-cart-with-options-stepper-layout',
-				true
-			);
-		} );
 		test( 'has the stepper option visible', async ( {
 			admin,
 			editor,
