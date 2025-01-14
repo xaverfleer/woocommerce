6c87b9b0c5a476ee513d2b0ec61111255a743f27
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-01-14 10:36:52 +0800

    [Settings] Load external scripts (#54112)
    * Enhance settings script management in WooCommerce admin
    
    - Introduced a mechanism to ignore specific scripts when loading settings pages.
    - Added functionality to capture and return script sources for each settings page.
    - Implemented a new private method `get_scripts_sources` to streamline script source retrieval.
    
    These changes improve the efficiency of script handling in the WooCommerce settings interface.
    
    * Enhance settings script management in WooCommerce admin
    
    - Added useLayoutEffect to dynamically append and remove settings scripts based on active page and section.
    - Introduced appendSettingsScripts and removeSettingsScripts functions for better script handling.
    - Improved integration with existing SETTINGS_SCRIPTS for more efficient script loading.
    
    These changes streamline the management of scripts in the WooCommerce settings interface.
    
    * Update ignore scripts
    
    * Improve scripts injection
    
    * Add changefile(s) from automation for the following project(s): woocommerce, woocommerce/client/admin
    
    * Add changefile(s) from automation for the following project(s): @woocommerce/settings-editor, woocommerce, woocommerce/client/admin
    
    * Add changefile(s) from automation for the following project(s): woocommerce, woocommerce/client/admin
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/client/admin/client/settings/index.js
+++ plugins/woocommerce/client/admin/client/settings/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { createRoot, useEffect } from '@wordpress/element';
+import { createRoot, useEffect, useLayoutEffect } from '@wordpress/element';
 import {
 	SettingsEditor,
 	useActiveRoute,
@@ -19,12 +19,48 @@ import './settings.scss';
 
 const node = document.getElementById( 'wc-settings-page' );
 
+const appendSettingsScripts = ( scripts ) => {
+	return scripts.map( ( script ) => {
+		const scriptElement = document.createElement( 'script' );
+		scriptElement.src = script;
+		scriptElement.onerror = () => {
+			// eslint-disable-next-line no-console
+			console.error( `Failed to load script: ${ script }` );
+		};
+		document.body.appendChild( scriptElement );
+		return scriptElement;
+	} );
+};
+
+const removeSettingsScripts = ( scripts ) => {
+	scripts.forEach( ( script ) => {
+		document.body.removeChild( script );
+	} );
+};
+
+const SETTINGS_SCRIPTS = window.wcSettings?.admin?.settingsScripts || [];
+
 registerTaxSettingsConflictErrorFill();
 registerPaymentsSettingsBannerFill();
 registerSiteVisibilitySlotFill();
 
 const Settings = () => {
 	const { activePage, activeSection } = useActiveRoute();
+
+	useLayoutEffect( () => {
+		const scripts = Array.from(
+			new Set( [
+				...( SETTINGS_SCRIPTS._default || [] ),
+				...( SETTINGS_SCRIPTS[ activePage ] || [] ),
+			] )
+		);
+
+		const scriptsElements = appendSettingsScripts( scripts );
+
+		return () => {
+			removeSettingsScripts( scriptsElements );
+		};
+	}, [ activePage, activeSection ] );
 
 	// Render the settings slots every time the page or section changes.
 	useEffect( () => {
