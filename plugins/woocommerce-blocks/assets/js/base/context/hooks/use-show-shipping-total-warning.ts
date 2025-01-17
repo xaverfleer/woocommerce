f617c09681748acf385decd8ed80ee6c3ecc6095
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-01-17 15:10:53 +0000

    Prevent a "Maximum update depth exceeded" error when adding notices to the `woocommerce/checkout-totals-block` context (#54380)

--- plugins/woocommerce-blocks/assets/js/base/context/hooks/use-show-shipping-total-warning.ts
+++ plugins/woocommerce-blocks/assets/js/base/context/hooks/use-show-shipping-total-warning.ts
@@ -6,6 +6,8 @@ import { CART_STORE_KEY, CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
 import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { hasShippingRate } from '@woocommerce/base-utils';
+import { store as noticesStore } from '@wordpress/notices';
+import type { WPNotice } from '@wordpress/notices/build-types/store/selectors';
 
 /**
  * Internal dependencies
@@ -25,13 +27,17 @@ export const useShowShippingTotalWarning = () => {
 					select( CHECKOUT_STORE_KEY ).prefersCollection(),
 				isRateBeingSelected:
 					select( CART_STORE_KEY ).isShippingRateBeingSelected(),
-				shippingNotices: select( 'core/notices' ).getNotices( context ),
+				shippingNotices: select( noticesStore ).getNotices( context ),
 			};
-		} );
-	const { createInfoNotice, removeNotice } = useDispatch( 'core/notices' );
+		}, [] );
+	const { createInfoNotice, removeNotice } = useDispatch( noticesStore );
 
 	useEffect( () => {
-		const isShowingNotice = shippingNotices.length > 0;
+		const isShowingNotice =
+			shippingNotices.length > 0 &&
+			shippingNotices.some(
+				( notice: WPNotice ) => notice.id === errorNoticeId
+			);
 		const hasMismatch = ! prefersCollection && hasSelectedLocalPickup;
 
 		if ( ! hasRates || isRateBeingSelected ) {
