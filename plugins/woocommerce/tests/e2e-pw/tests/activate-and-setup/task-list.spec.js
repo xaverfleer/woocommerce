e067c861bd3f0630fc1d213ace4a378131cb4491
rodelgc <od.rodel.calasagsag@gmail.com>
2025-01-27 15:54:02 +0800

    Unskip on WPCOM and Pressable: `activate-and-setup/task-list.spec.js` (#54771)
    * Make test runnable on WPCOM and Pressable
    
    * Add changelog.

--- plugins/woocommerce/tests/e2e-pw/tests/activate-and-setup/task-list.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/activate-and-setup/task-list.spec.js
@@ -5,57 +5,17 @@ const { exec } = require( 'child_process' );
 test.describe( 'WC Home Task List >', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 
-	test.beforeAll( async ( {} ) => {
-		return new Promise( ( resolve, reject ) => {
-			const deleteCommand = `wp-env run tests-cli wp option delete woocommerce_task_list_hidden_lists --allow-root`;
-			exec( deleteCommand, ( error, stdout, stderr ) => {
-				if ( error ) {
-					console.error(
-						`Error deleting option: ${ error.message }`
-					);
-					return reject( error );
-				}
-				if ( stderr && ! stderr.includes( 'Ran `wp option delete' ) ) {
-					console.error( `Error output: ${ stderr }` );
-					return reject( new Error( stderr ) );
-				}
-				console.log( 'Option deleted successfully.' );
-				resolve( stdout );
-			} );
-		} );
-	} );
-
-	test.afterAll( async ( {} ) => {
-		return new Promise( ( resolve, reject ) => {
-			const deleteCommand = `wp-env run tests-cli wp option delete woocommerce_task_list_hidden_lists --allow-root`;
-			exec( deleteCommand, ( error, stdout, stderr ) => {
-				if ( error ) {
-					console.error(
-						`Error deleting option: ${ error.message }`
-					);
-					return reject( error );
-				}
-				if ( stderr && ! stderr.includes( 'Ran `wp option delete' ) ) {
-					console.error( `Error output: ${ stderr }` );
-					return reject( new Error( stderr ) );
-				}
-				console.log( 'Option deleted successfully.' );
-				resolve( stdout );
-			} );
-		} );
-	} );
-
 	// TODO (E2E Audit): This test should be combined with other WC Homepage setup tests like the tests in activate-and-setup/stats-overview.spec.js into a single spec.
 	test(
 		'Can hide the task list',
-		{ tag: [ tags.SKIP_ON_PRESSABLE, tags.SKIP_ON_WPCOM, tags.NOT_E2E ] },
+		{ tag: [ tags.NOT_E2E ] },
 		async ( { page } ) => {
-			await test.step( 'Load the WC Admin page', async () => {
-				await page.goto( 'wp-admin/admin.php?page=wc-admin' );
+			await test.step( 'Load the WC Admin page with the task list enabled', async () => {
+				await page.goto(
+					'wp-admin/admin.php?page=wc-admin&reset_task_list=1'
+				);
 				await expect(
-					page.getByRole( 'heading', {
-						name: 'Start customizing your store',
-					} )
+					page.getByText( 'Customize your store' )
 				).toBeVisible();
 				await expect(
 					page.getByText( 'Store management' )
@@ -78,6 +38,12 @@ test.describe( 'WC Home Task List >', () => {
 				await expect(
 					page.getByText( 'Store management' )
 				).toBeVisible();
+			} );
+
+			await test.step( 'Re-enable task list', async () => {
+				await page.goto(
+					'wp-admin/admin.php?page=wc-admin&reset_task_list=1'
+				);
 			} );
 		}
 	);
