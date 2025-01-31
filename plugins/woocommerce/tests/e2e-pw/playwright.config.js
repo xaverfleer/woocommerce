045c8a8fd11d0cf6ad26d9cc61b47fa1237e5e1f
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-01-31 15:11:19 +0200

    [e2e tests] Remove left over Playwright project from main config file (#55013)

--- plugins/woocommerce/tests/e2e-pw/playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/playwright.config.js
@@ -2,10 +2,6 @@
  * External dependencies
  */
 import { defineConfig, devices } from '@playwright/test';
-/**
- * Internal dependencies
- */
-import { tags } from './fixtures/fixtures';
 
 require( 'dotenv' ).config( { path: __dirname + '/.env' } );
 
@@ -137,10 +133,6 @@ export default defineConfig( {
 			name: 'api',
 			testMatch: '**/api-tests/**',
 			dependencies: [ 'site setup' ],
-		},
-		{
-			name: 'e2e-hpos-disabled',
-			grep: new RegExp( tags.HPOS ),
 		},
 	],
 } );
