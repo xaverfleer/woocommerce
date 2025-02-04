0135e4943791d9ba8a6281c0f453d6b9754c0225
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2025-02-04 13:27:36 -0800

    WC_Order: Ensure status is a string in the setter (#54861)
    Ensure status is a string in the setter
    
    This simply ensures type safety when setting an order status, before
    using the `OrderUtil::remove_status_prefix`, which requires a string
    for its input. Necessary when using more modern code within older parts
    of the codebase that were written before type hinting was widely
    available in PHP.
    
    This also updates the linebreaks and whitespace in two very complex
    conditionals within the same method, for better readability.
    
    Fixes #54855

--- plugins/woocommerce/includes/abstracts/abstract-wc-order.php
+++ plugins/woocommerce/includes/abstracts/abstract-wc-order.php
@@ -651,19 +651,31 @@ abstract class WC_Abstract_Order extends WC_Abstract_Legacy_Order {
 	 */
 	public function set_status( $new_status ) {
 		$old_status = $this->get_status();
-		$new_status = OrderUtil::remove_status_prefix( $new_status );
+		$new_status = OrderUtil::remove_status_prefix( (string) $new_status );
 
 		$status_exceptions = array( OrderStatus::AUTO_DRAFT, OrderStatus::TRASH );
 
 		// If setting the status, ensure it's set to a valid status.
 		if ( true === $this->object_read ) {
 			// Only allow valid new status.
-			if ( ! in_array( 'wc-' . $new_status, $this->get_valid_statuses(), true ) && ! in_array( $new_status, $status_exceptions, true ) ) {
+			if (
+				! in_array( 'wc-' . $new_status, $this->get_valid_statuses(), true )
+				&& ! in_array( $new_status, $status_exceptions, true )
+			) {
 				$new_status = OrderStatus::PENDING;
 			}
 
 			// If the old status is set but unknown (e.g. draft) assume its pending for action usage.
-			if ( $old_status && ( OrderStatus::AUTO_DRAFT === $old_status || ( ! in_array( 'wc-' . $old_status, $this->get_valid_statuses(), true ) && ! in_array( $old_status, $status_exceptions, true ) ) ) ) {
+			if (
+				$old_status
+				&& (
+					OrderStatus::AUTO_DRAFT === $old_status
+					|| (
+						! in_array( 'wc-' . $old_status, $this->get_valid_statuses(), true )
+						&& ! in_array( $old_status, $status_exceptions, true )
+					)
+				)
+			) {
 				$old_status = OrderStatus::PENDING;
 			}
 		}
