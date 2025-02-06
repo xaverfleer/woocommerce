09f927135530773b880183b8729d2a3fe1a8fa8b
rodelgc <od.rodel.calasagsag@gmail.com>
2025-02-06 13:38:30 +0800

    Unskip on WPCOM: `shopper/checkout-block.spec.js` (#54766)
    * Replace hardcoded customer ID with the correct one.
    
    * Organize beforeAll and beforeEach into distinct steps to greatly help with debugging.
    
    * Add changelog
    
    * Remove redundant step of disabling 'Coming Soon' feature
    
    * Add setup file for getting admin and customer user IDs.
    
    * Replace wcApi instantiations with fixture.
    
    * Revert "Add setup file for getting admin and customer user IDs."
    
    This reverts commit 710c90c9881b558b47d4e09f4c85b3542591b8b0.
    
    * Get customer ID as additional step in beforeAll.

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/checkout-block.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/checkout-block.spec.js
@@ -11,7 +11,6 @@ import {
 	fillBillingCheckoutBlocks,
 } from '@woocommerce/e2e-utils-playwright';
 import { request } from '@playwright/test';
-import wcApi from '@woocommerce/woocommerce-rest-api';
 
 /**
  * Internal dependencies
@@ -84,142 +83,154 @@ const test = baseTest.extend( {
 
 test.describe(
 	'Checkout Block page',
-	{ tag: [ tags.PAYMENTS, tags.SERVICES, tags.HPOS, tags.SKIP_ON_WPCOM ] },
+	{ tag: [ tags.PAYMENTS, tags.SERVICES, tags.HPOS ] },
 	() => {
-		test.beforeAll( async ( { baseURL } ) => {
-			const api = new wcApi( {
-				url: baseURL,
-				consumerKey: process.env.CONSUMER_KEY,
-				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
+		test.beforeAll( async ( { baseURL, api } ) => {
+			await test.step( 'Set field visibility options', async () => {
+				await setOption(
+					request,
+					baseURL,
+					'woocommerce_checkout_phone_field',
+					'optional'
+				);
+				await setOption(
+					request,
+					baseURL,
+					'woocommerce_checkout_company_field',
+					'optional'
+				);
+				await setOption(
+					request,
+					baseURL,
+					'woocommerce_checkout_address_2_field',
+					'optional'
+				);
 			} );
-			// Set field visibility options
-			await setOption(
-				request,
-				baseURL,
-				'woocommerce_checkout_phone_field',
-				'optional'
-			);
-			await setOption(
-				request,
-				baseURL,
-				'woocommerce_checkout_company_field',
-				'optional'
-			);
-			await setOption(
-				request,
-				baseURL,
-				'woocommerce_checkout_address_2_field',
-				'optional'
-			);
-			// make sure the currency is USD
-			await api.put( 'settings/general/woocommerce_currency', {
-				value: 'USD',
-			} );
-			// add product
-			await api
-				.post( 'products', {
-					name: simpleProductName,
-					description: simpleProductDesc,
-					type: 'simple',
-					regular_price: singleProductFullPrice,
-					sale_price: singleProductSalePrice,
-				} )
-				.then( ( response ) => {
-					productId = response.data.id;
+
+			await test.step( 'Make sure the currency is USD', async () => {
+				await api.put( 'settings/general/woocommerce_currency', {
+					value: 'USD',
 				} );
-			// enable logging through checkout
-			await api.put(
-				'settings/account/woocommerce_enable_checkout_login_reminder',
-				{
-					value: 'yes',
-				}
-			);
-			// enable creating account through checkout
-			await api.put(
-				'settings/account/woocommerce_enable_signup_and_login_from_checkout',
-				{
-					value: 'yes',
-				}
-			);
-			// add a shipping zone and method
-			await api
-				.post( 'shipping/zones', {
-					name: 'California and Oregon Shipping Zone',
-				} )
-				.then( ( response ) => {
-					shippingZoneId = response.data.id;
+			} );
+
+			await test.step( 'Add test product.', async () => {
+				await api
+					.post( 'products', {
+						name: simpleProductName,
+						description: simpleProductDesc,
+						type: 'simple',
+						regular_price: singleProductFullPrice,
+						sale_price: singleProductSalePrice,
+					} )
+					.then( ( response ) => {
+						productId = response.data.id;
+					} );
+			} );
+
+			await test.step( 'Enable logging through checkout', async () => {
+				await api.put(
+					'settings/account/woocommerce_enable_checkout_login_reminder',
+					{
+						value: 'yes',
+					}
+				);
+			} );
+
+			await test.step( 'Enable creating account through checkout', async () => {
+				await api.put(
+					'settings/account/woocommerce_enable_signup_and_login_from_checkout',
+					{
+						value: 'yes',
+					}
+				);
+			} );
+
+			await test.step( 'Add a shipping zone and method', async () => {
+				await api
+					.post( 'shipping/zones', {
+						name: 'California and Oregon Shipping Zone',
+					} )
+					.then( ( response ) => {
+						shippingZoneId = response.data.id;
+					} );
+				await api.put( `shipping/zones/${ shippingZoneId }/locations`, [
+					{
+						code: 'US:CA',
+						type: 'state',
+					},
+					{
+						code: 'US:OR',
+						type: 'state',
+					},
+				] );
+				await api.post( `shipping/zones/${ shippingZoneId }/methods`, {
+					method_id: 'free_shipping',
+					settings: {
+						title: 'Free shipping',
+					},
 				} );
-			await api.put( `shipping/zones/${ shippingZoneId }/locations`, [
-				{
-					code: 'US:CA',
-					type: 'state',
-				},
-				{
-					code: 'US:OR',
-					type: 'state',
-				},
-			] );
-			await api.post( `shipping/zones/${ shippingZoneId }/methods`, {
-				method_id: 'free_shipping',
-				settings: {
-					title: 'Free shipping',
-				},
+				await api.post( `shipping/zones/${ shippingZoneId }/methods`, {
+					method_id: 'local_pickup',
+					settings: {
+						title: 'Local pickup',
+					},
+				} );
+				await api.post( `shipping/zones/${ shippingZoneId }/methods`, {
+					method_id: 'flat_rate',
+					settings: {
+						title: 'Flat rate',
+						cost: singleProductSalePrice,
+					},
+				} );
 			} );
-			await api.post( `shipping/zones/${ shippingZoneId }/methods`, {
-				method_id: 'local_pickup',
-				settings: {
-					title: 'Local pickup',
-				},
+
+			await test.step( 'Enable bank transfers and COD for payment', async () => {
+				await api.put( 'payment_gateways/bacs', {
+					enabled: true,
+				} );
+				await api.put( 'payment_gateways/cod', {
+					enabled: true,
+				} );
 			} );
-			await api.post( `shipping/zones/${ shippingZoneId }/methods`, {
-				method_id: 'flat_rate',
-				settings: {
-					title: 'Flat rate',
-					cost: singleProductSalePrice,
-				},
-			} );
-			// enable bank transfers and COD for payment
-			await api.put( 'payment_gateways/bacs', {
-				enabled: true,
-			} );
-			await api.put( 'payment_gateways/cod', {
-				enabled: true,
-			} );
-			// make sure our customer user has a pre-defined billing/shipping address
-			await api.put( `customers/2`, {
-				shipping: {
-					first_name: 'Maggie',
-					last_name: 'Simpson',
-					company: '',
-					address_1: '123 Evergreen Terrace',
-					address_2: '',
-					city: 'Springfield',
-					state: 'OR',
-					postcode: '97403',
-					country: 'US',
-				},
-				billing: {
-					first_name: 'Maggie',
-					last_name: 'Simpson',
-					company: '',
-					address_1: '123 Evergreen Terrace',
-					address_2: '',
-					city: 'Springfield',
-					state: 'OR',
-					postcode: '97403',
-					country: 'US',
-				},
+
+			await test.step( 'Make sure our customer user has a pre-defined billing/shipping address', async () => {
+				// Get existing customer ID
+				const { data } = await api.get( 'customers', {
+					email: customer.email,
+					role: 'all',
+				} );
+				expect( data ).toMatchObject( [ { email: customer.email } ] );
+
+				// Update its billing & shipping address
+				const customerId = data[ 0 ].id;
+				await api.put( `customers/${ customerId }`, {
+					shipping: {
+						first_name: 'Maggie',
+						last_name: 'Simpson',
+						company: '',
+						address_1: '123 Evergreen Terrace',
+						address_2: '',
+						city: 'Springfield',
+						state: 'OR',
+						postcode: '97403',
+						country: 'US',
+					},
+					billing: {
+						first_name: 'Maggie',
+						last_name: 'Simpson',
+						company: '',
+						address_1: '123 Evergreen Terrace',
+						address_2: '',
+						city: 'Springfield',
+						state: 'OR',
+						postcode: '97403',
+						country: 'US',
+					},
+				} );
 			} );
 		} );
 
-		test.afterAll( async ( { baseURL } ) => {
-			const api = new wcApi( {
-				url: baseURL,
-				consumerKey: process.env.CONSUMER_KEY,
-				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
-			} );
+		test.afterAll( async ( { api } ) => {
 			await api.delete( `products/${ productId }`, {
 				force: true,
 			} );
@@ -291,33 +302,28 @@ test.describe(
 			} );
 		} );
 
-		test.beforeEach( async ( { baseURL } ) => {
-			const api = new wcApi( {
-				url: baseURL,
-				consumerKey: process.env.CONSUMER_KEY,
-				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
-			} );
-			// ensure the store address is always in the US
-			await api.post( 'settings/general/batch', {
-				update: [
-					{
-						id: 'woocommerce_store_address',
-						value: 'addr 1',
-					},
-					{
-						id: 'woocommerce_store_city',
-						value: 'San Francisco',
-					},
-					{
-						id: 'woocommerce_default_country',
-						value: 'US:CA',
-					},
-					{
-						id: 'woocommerce_store_postcode',
-						value: '94107',
-					},
-				],
+		test.beforeEach( async ( { api } ) => {
+			await test.step( 'Ensure the store address is always in the US', async () => {
+				await api.post( 'settings/general/batch', {
+					update: [
+						{
+							id: 'woocommerce_store_address',
+							value: 'addr 1',
+						},
+						{
+							id: 'woocommerce_store_city',
+							value: 'San Francisco',
+						},
+						{
+							id: 'woocommerce_default_country',
+							value: 'US:CA',
+						},
+						{
+							id: 'woocommerce_store_postcode',
+							value: '94107',
+						},
+					],
+				} );
 			} );
 		} );
 
@@ -987,14 +993,8 @@ test.describe(
 		test( 'can create an account during checkout with custom password', async ( {
 			page,
 			testPage,
-			baseURL,
+			api,
 		} ) => {
-			const api = new wcApi( {
-				url: baseURL,
-				consumerKey: process.env.CONSUMER_KEY,
-				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
-			} );
 			// Password generation off
 			await api.put(
 				'settings/account/woocommerce_registration_generate_password',
